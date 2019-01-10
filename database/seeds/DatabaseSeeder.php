1ed039df44e084555d4d20aaec2129dd134e9794
xiangqian <175023117@qq.com>
2019-01-10 14:07:40 +0800

    打开注释

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-//         $this->call(CreateDepartmentsSeeder::class);
-//         $this->call(CreateUsersSeeder::class);
+         $this->call(CreateDepartmentsSeeder::class);
+         $this->call(CreateUsersSeeder::class);
          $this->call(CreateStationeriesSeeder::class);
     }
 }
