546cdd09f0d7d30e2c4c4323f92958879c6a22ad
xiangqian <175023117@qq.com>
2019-01-23 10:02:39 +0800

    ajax实现查询快递信息

--- resources/views/admin/express.blade.php
+++ resources/views/admin/express.blade.php
@@ -1,6 +1,6 @@
 <style>
   .status {
-    background: url(/img/spider_search_v4.png) 70px -725px no-repeat;
+    background: url(/img/spider_search_v4.png) 100px -725px no-repeat;
   }
 </style>
 <div class="box box-info">
@@ -17,7 +17,7 @@
 
             <span class="input-group-addon"><i class="fa fa-pencil fa-fw"></i></span>
 
-            <input type="text" id="express_100" name="express_100" value="" class="form-control name"
+            <input type="text" id="express" name="express" value="" class="form-control name"
                    placeholder="请输入要查询的快递单号">
 
           </div>
@@ -25,7 +25,7 @@
         </div>
 
         <div class="btn-group ">
-          <button type="submit" class="btn btn-primary express_100">点击查询</button>
+          <button type="submit" class="btn btn-primary express">点击查询</button>
         </div>
         {{ csrf_field() }}
       </div>
@@ -40,26 +40,10 @@
       <table style="font-size: 18px;" class="table table-bordered text-center">
 
             <tr >
-              <th colspan="2">时间&nbsp;&nbsp;</th>
-              <th>状态</th>
-              <th colspan="3">&nbsp;&nbsp;地点和跟踪进度</th>
+              <th colspan="2">时间</th>
+              <th>快递信息</th>
+              <th colspan="3">地点和跟踪进度</th>
             </tr>
-            <tr>
-              <td  colspan="2">暂无数据</td>
-              <td class="status">&nbsp;&nbsp;&nbsp;&nbsp;</td>
-              <td colspan="3">暂无数据</td>
-            </tr>
-            <tr>
-              <td colspan="2">2019-01-09 12:11&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-              <td class="status">&nbsp;&nbsp;&nbsp;&nbsp;</td>
-              <td colspan="3">货物已交付京东物流</td>
-            </tr>
-            <tr>
-              <td colspan="2">2019-01-08 12:11&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-              <td class="status">&nbsp;&nbsp;&nbsp;&nbsp;</td>
-              <td colspan="3">货物已到达【成都青白江分拣中心】</td>
-            </tr>
-
 
       </table>
     </div>
@@ -67,23 +51,30 @@
 </div>
 
 <script>
-  $('.express_100').on('click', function (e) {
+  $('.express').on('click', function (e) {
 
-    if (!$('#express_100').val().trim()) {
+    if (!$('#express').val().trim()) {
       alert('查询单号必须填写')
       return
     }
 
     $.ajax({
-      url: '/admin/users/show',
+      url: '/admin/express/info',
       type: 'get',
       data: {
-        express: $('#express_100').val(),
-        channel: '100',
+        code: $('#express').val(),
       },
       success: function (res) {
-        console.log(res)
-        // alert(res.result)
+        var data = res.data[0]
+        var name = '快递:'+data.logistics_company
+        var item = "<tr><th colspan='2'>时间</th><th colspan='2'>"+name+"</th><th colspan='3'>地点和跟踪进度</th></tr>"
+        $.each(data.data,function(i,result){
+          item += "<tr><td colspan='2'>"+result['time']+"</td><td class='status'>&nbsp;</td><td colspan='3'>"+result['description']+"</td></tr>";
+        });
+        $('.table').html(item);
+      },
+      error : function (res) {
+        alert(res.responseJSON['message']);
       }
     })
   })
