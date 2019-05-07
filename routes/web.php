559be494468d769ea6c0c20c2d3047f3a0e39b30
xiangqian <175023117@qq.com>
2019-05-07 14:51:07 +0800

    注释web.php路由

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-//    return view('welcome');
-    return redirect('/admin/auth/login');
-});
+//Route::get('/', function () {
+////    return view('welcome');
+//    return redirect('/admin/auth/login');
+//});
