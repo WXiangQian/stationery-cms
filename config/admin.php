4c1a52aa42f0af12c0351eeb000addbfba2ee31d
xiangqian <175023117@qq.com>
2019-08-21 13:38:12 +0800

    更换登录页背景图配置

--- config/admin.php
+++ config/admin.php
@@ -252,7 +252,7 @@ return [
     | This value is used to set the background image of login page.
     |
     */
-    'login_background_image' => '',
+    'login_background_image' => '/img/3.png',
 
     /*
     |--------------------------------------------------------------------------
